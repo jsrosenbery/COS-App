8f6f074552c6100456eea2a8410fc6aaa270a912
jsrosenbery <jacoba@cos.edu>
2025-06-23 15:07:20 -0700

    Add files via upload

--- js/calendar.js
+++ js/calendar.js
@@ -1,20 +1,19 @@
 export function initCalendar(data) {
   const calendarEl = document.getElementById('calendar');
-  if (window.FullCalendar) {
-    const events = data.flatMap(r => r.Days.map(day => ({
-      title: `${r.Building}-${r.Room}`,
-      startRecur: r.Start_Date,
-      endRecur: r.End_Date,
-      daysOfWeek: [ ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'].indexOf(day) ],
-      startTime: r.Start_Time,
-      endTime: r.End_Time
-    })));
+  const events = data.flatMap(r => r.Days.map(day => ({
+    title: `${r.Building}-${r.Room}`,
+    startRecur: r.Start_Date,
+    endRecur: r.End_Date,
+    daysOfWeek: [ ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'].indexOf(day) ],
+    startTime: r.Start_Time,
+    endTime: r.End_Time
+  })));
 
-    new FullCalendar.Calendar(calendarEl, {
-      initialView: 'timeGridWeek',
-      plugins: [ FullCalendar.TimeGridPlugin ],
-      weekNumbers: true,
-      events
-    }).render();
-  }
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'timeGridWeek',
+    weekNumbers: true,
+    events
+  });
+
+  calendar.render();
 }
