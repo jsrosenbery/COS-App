b0c8186fe1be500e1819bfabf65fdc20d892a5b0
jsrosenbery <jacoba@cos.edu>
2025-06-23 15:10:30 -0700

    Add files via upload

--- js/heatmap.js
+++ js/heatmap.js
@@ -2,12 +2,8 @@ import { getTimeRangeFromData, parseHour } from './timeUtils.js';
 
 export function renderHeatmap(data) {
   const container = document.getElementById('heatmap-container');
-  container.innerHTML = '';
-
+  // Simple stub: display parsed data
   const { start, end } = getTimeRangeFromData(data);
-  // Build heatmap grid based on start/end and room occupancy
-  // [Implementation here]
-
-  // Append generated heatmap element
-  container.appendChild(hmTable);
+  container.innerHTML = '<h3>Parsed Entries: ' + data.length + '</h3>'
+    + '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
 }
